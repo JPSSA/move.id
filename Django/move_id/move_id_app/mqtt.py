85a09d2fe7dca8d7badbfb68b0e46efcd09dc923
Hugo Ferreira <hugo.dn.ferreira@gmail.com>
2024-03-20 17:13:40 +0000

    connect only one client

--- Django/move_id/move_id_app/mqtt.py
+++ Django/move_id/move_id_app/mqtt.py
@@ -1,15 +1,25 @@
 import paho.mqtt.client as mqtt
 from django.conf import settings
 
+subscribed_to_topic = False
+
+
 # MQTT client configuration
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
 
 def on_connect(mqtt_client, userdata, flags, rc):
+     
     if rc == 0:
         print('Connected successfully')
-        mqtt_client.subscribe('django/move_id_test')
+        # Check if not already subscribed to the topic
+        if not subscribed_to_topic:
+            mqtt_client.subscribe('django/move_id_test')
+            subscribed_to_topic = True  # Update the flag to indicate subscription
+        else:
+            print('Already subscribed to the topic')
     else:
         print('Bad connection. Code:', rc)
+   
 
 def on_message(mqtt_client, userdata, msg):
     print(f'Received message on topic: {msg.topic} with payload: {msg.payload}')
