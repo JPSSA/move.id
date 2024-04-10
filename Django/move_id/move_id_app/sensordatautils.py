f16c06d8cbcb14c6705cef5f5f135a4d1309aa38
Hugo Ferreira <hugo.dn.ferreira@gmail.com>
2024-04-10 15:31:24 +0100

    delete methods of no use

--- Django/move_id/move_id_app/sensordatautils.py
+++ Django/move_id/move_id_app/sensordatautils.py
@@ -32,42 +32,7 @@ def get_sensor_data_as_dataframe(topic_id):
     
     return sensor_data_df
 
-def get_recent_values_accelerometer(topic_id, axis, num_values=6):
-    axis_mapping = {
-        'x': 'accelerometerX',
-        'y': 'accelerometerY',
-        'z': 'accelerometerZ'
-    }
-
-    if axis not in axis_mapping:
-        raise ValueError("Invalid axis value. Valid values are 'x', 'y', or 'z'.")
-
-    recent_data = SensorData.objects.filter(topic_id=topic_id).order_by('-datetime')[:num_values]
-
-    axis_field = axis_mapping[axis]
-    axis_values = [getattr(data, axis_field) for data in recent_data if getattr(data, axis_field) is not None]
-
-    return axis_values
-
-def get_recent_values_gyroscope(topic_id, axis, num_values=6):
-    axis_mapping = {
-        'x': 'gyroscopeX',
-        'y': 'gyroscopeY',
-        'z': 'gyroscopeZ'
-    }
-
-    if axis not in axis_mapping:
-        raise ValueError("Invalid axis value. Valid values are 'x', 'y', or 'z'.")
-
-    recent_data = SensorData.objects.filter(topic_id=topic_id).order_by('-datetime')[:num_values]
-
-    axis_field = axis_mapping[axis]
-    axis_values = [getattr(data, axis_field) for data in recent_data if getattr(data, axis_field) is not None]
-
-    return axis_values
-
 def appendData(msg):
-    
     sensor_data_instance = SensorData.objects.create(
         datetime = datetime.now(),
         topic_id = msg.topic,
