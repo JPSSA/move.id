3b84411939792ece30875e1a2ceede5743127c52
Hugo Ferreira <hugo.dn.ferreira@gmail.com>
2024-04-24 15:11:42 +0100

    admin site regiester

--- Django/move_id/move_id_app/admin.py
+++ Django/move_id/move_id_app/admin.py
@@ -3,16 +3,17 @@ from .models import Patient
 from .models import SensorData
 from .models import Sensor
 from .models import Classifier
-from .models import DatasetAttributes
 from .models import Dataset
 from .models import UserSensor
+from .models import Location
 
 admin.site.register(Patient)
 admin.site.register(SensorData)
+admin.site.register(Location)
 admin.site.register(Sensor)
-admin.site.register(Classifier)
-admin.site.register(DatasetAttributes)
-admin.site.register(Dataset)
 admin.site.register(UserSensor)
+admin.site.register(Classifier)
+admin.site.register(Dataset)
+
 
 
