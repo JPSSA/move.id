7560fdff24c48c973d1268057d50c8f9a3036d42
joaopereira76 <joao.pereira197606@gmail.com>
2024-07-01 19:07:04 +0100

    modified:   Django/move_id/move_id_app/classifiers/histogram_based_outlier_detection.py

--- Django/move_id/move_id_app/classifiers/histogram_based_outlier_detection.py
+++ Django/move_id/move_id_app/classifiers/histogram_based_outlier_detection.py
@@ -2,23 +2,23 @@ from move_id_app.classifiers.classifiers import OneClassClassifier
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.preprocessing import StandardScaler
 
-class HistogramBasedOutlierDetection(OneClassClassifier):
+class HistogramBasedOutlierDetection(Classifier):
     
     def __init__(self):
         super().__init__(0.7,HBOS, {})
         self.scaler = StandardScaler()
 
     def fit(self,X_train):
-        
         self.scaler.fit(X_train)
         X_train_scalled = self.scaler.transform(X_train)
         self.model.fit(X_train_scalled)
 
     
     def predict(self,X):
+        X_test_scalled = self.scaler.transform(X)
         scores = model.decision_function(X_test_scalled)        
         threshold = np.percentile(scores, p)  
-        predicitons = -(scores > threshold).astype(int)
+        predicitons = -(scores > 90).astype(int)
         y_pred = [ -1 if p == -1 else 1 for p in predicitons]
         for y in y_pred:
             if(y == -1):
